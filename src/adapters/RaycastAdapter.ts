5659e4be9b172c553c5b6a90b8ae457d5c9dd2a2
Vladimir <vla.grishenko@yandex.ru>
2025-02-25 09:13:05 +0300

    move raycast logic to adapter, scene refator

---
+++ src/adapters/RaycastAdapter.ts
@@ -0,0 +1,37 @@
+import { PerspectiveCamera, Raycaster, Renderer, Vector2, Scene as ThreeScene, } from "three";
+import Scene from "../domain/Scene";
+
+export class RaycastAdapter {
+    #renderer: Renderer;
+    #raycaster = new Raycaster();
+    #camera: PerspectiveCamera;
+    #mouseCoordVector = new Vector2()
+    #scene: ThreeScene;
+    
+    constructor(scene: Scene) {
+        this.#scene = scene.getScene();
+        this.#renderer = scene.getRenderer();
+        this.#camera = scene.getCamera();
+
+        this.initListener()
+    }
+
+    private initListener() {
+        this.#renderer.domElement.addEventListener('click', this.raycast.bind(this), false);
+    }
+
+    private raycast(event: any) {
+        console.log('raycast');
+        this.#mouseCoordVector.x = (event.clientX / window.innerWidth) * 2 - 1;
+        this.#mouseCoordVector.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        this.#raycaster.setFromCamera(this.#mouseCoordVector, this.#camera);
+        
+        const intersects = this.#raycaster.intersectObjects(this.#scene.children);
+        const intersect = intersects.length && intersects[0];
+        
+        console.log({ intersect });
+        if (intersect && intersect.object.parent && intersect.object.parent.name !== '') {
+            console.log(intersect.object.parent);
+        }
+    }
+}
