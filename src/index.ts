e618f9ca77c860f4fe5cb89a6cd50e648b55dc57
Vladimir <boooobka@gmail.com>
2025-09-17 22:12:35 +0300

    move init widgets to widgets floader, async app init

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,13 @@
 import './index.css';
-import { sceneAdater } from './chess/adapters/SceneAdapter';
-import { Application } from './chess/application/Application';
-import type { CreateAppFunction } from 'vue';
-import type { Pinia } from 'pinia';
 
-new Application(sceneAdater);
+import('./chess/adapters/SceneAdapter')
+.then(({sceneAdater}) => 
+	import('./chess/application/Application')
+	.then(({ Application })=> new Application(sceneAdater))
+	.catch(e => console.error(e))
+)
+.catch(e => console.error(e));
 
-const initWidget = async (pinia: Pinia, createApp: CreateAppFunction<Element>, widgetName: string, mountId: string) => {
-	 import(`./widgets/${widgetName}/${widgetName}.vue`).then(({ default: Widget }) => {
-		const app = createApp(Widget);
-		app.use(pinia)
-		app.mount(mountId);
-	 }).catch(e => console.error(e));
-}
-
-const widgets = [
-	{
-		name: 'History',
-		mountId: '#history'
-	},
-	{
-		name: 'CuttedFigures',
-		mountId: '#cuttedFigures'
-	},
-	{
-		name: 'Rollback',
-		mountId: '#rollback'
-	}
-]
-
-const initWidgets = async () => {
-	try {
-		const { createApp } = await import('vue');
-		const { createPinia } = await import('pinia');
-		const pinia = createPinia();
-		widgets.forEach(widget => initWidget(pinia, createApp, widget.name, widget.mountId));
-	} catch(e) {
-		console.error(e);
-	}
-}
-
-initWidgets();
+import('./widgets/initWigets')
+.then(({ initWidgets }) => initWidgets())
+.catch(e => console.error(e));
---
+++ src/widgets/initWigets.ts
@@ -0,0 +1,36 @@
+import type { Pinia } from 'pinia';
+import type { CreateAppFunction } from 'vue';
+
+const initWidget = async (pinia: Pinia, createApp: CreateAppFunction<Element>, widgetName: string, mountId: string) => {
+	 import(`./${widgetName}/${widgetName}.vue`).then(({ default: Widget }) => {
+		const app = createApp(Widget);
+		app.use(pinia)
+		app.mount(mountId);
+	 }).catch(e => console.error(e));
+}
+
+const widgets = [
+	{
+		name: 'History',
+		mountId: '#history'
+	},
+	{
+		name: 'CuttedFigures',
+		mountId: '#cuttedFigures'
+	},
+	{
+		name: 'Rollback',
+		mountId: '#rollback'
+	}
+]
+
+export const initWidgets = async () => {
+	try {
+		const { createApp } = await import('vue');
+		const { createPinia } = await import('pinia');
+		const pinia = createPinia();
+		widgets.forEach(widget => initWidget(pinia, createApp, widget.name, widget.mountId));
+	} catch(e) {
+		console.error(e);
+	}
+}
