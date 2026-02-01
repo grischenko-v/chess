1479e210fc0fb3287684d267c2cbf3e9801b9fcb
Vladimir <boooobka@gmail.com>
2026-02-01 20:39:14 +0300

    change AI engine to stockfish.js singlecore light wasm version

--- src/AIBot/AIBot.ts
+++ src/AIBot/AIBot.ts
@@ -1,15 +1,10 @@
-
-
-const WASM_TEST_BYTES = Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00);
-const WASM_BOT_WORKER_PATH = '/chess/dist/stockfish/stockfish.wasm.js';
-const JS_BOT_WORKER_PATH = '/chess/dist/public/stockfish/stockfish.js';
+const JS_BOT_WORKER_PATH = '/chess/dist/public/stockfish/stockfish-17.1-lite-single-03e3232.js';
 
 export class AIBot {
 	botWorker: Worker;
 	
 	constructor() {
-		const wasmSupported = this.checkWasmSupported();
-		this.botWorker = new Worker(wasmSupported ? WASM_BOT_WORKER_PATH : JS_BOT_WORKER_PATH);
+		this.botWorker = new Worker(JS_BOT_WORKER_PATH);
 	}
 
 	addListeners(onMessage: (msg: string) => void) {
@@ -20,9 +15,5 @@ export class AIBot {
 
 	message(msg: string) {
 		this.botWorker.postMessage(msg);
-	}
-
-	checkWasmSupported() {
-		return typeof WebAssembly === 'object' && WebAssembly.validate(WASM_TEST_BYTES);
 	}
 }
