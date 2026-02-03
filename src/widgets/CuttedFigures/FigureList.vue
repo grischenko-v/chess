cfde37d9bc350701d12ac638d99edb76b7035f60
Vladimir <boooobka@gmail.com>
2026-02-03 15:08:50 +0300

    types

--- src/widgets/CuttedFigures/FigureList.vue
+++ src/widgets/CuttedFigures/FigureList.vue
@@ -35,7 +35,7 @@ type Props = {
 
 defineProps<Props>()
 
-function getCupturedFigureIcon(figure: {figureType: string, figureColor: FigureColor}) {
+function getCupturedFigureIcon(figure: {figureType: FigureType, figureColor: FigureColor}) {
   return `/chess/dist/imgs/figures/${figure.figureType}${figure.figureColor}.png`;
 }
 </script>
